4f2e4596782b13f7fdf3b3ce0b417a9e54804673
Dean Menso <deanmenso@gmail.com>
2017-07-05 13:25:40 -0400

    constants

--- public/src/scripts/services/constants.js
+++ public/src/scripts/services/constants.js
@@ -3,9 +3,9 @@
 
     angular.module('sande')
         .constant('constants', {
-            // APP_SERVER: 'https://sande-test.herokuapp.com/api',
-            // APP_SOCKET: 'https://sande-test.herokuapp.com',
-            APP_SERVER: 'http://localhost:3000/api',
-            APP_SOCKET: 'http://localhost:3000'
+            APP_SERVER: 'https://sande-test.herokuapp.com/api',
+            APP_SOCKET: 'https://sande-test.herokuapp.com',
+            // APP_SERVER: 'http://localhost:3000/api',
+            // APP_SOCKET: 'http://localhost:3000'
         });
 }());
